a25038c5512dd349771a6538df0afd6689342535
Victor Mutiso <mutisovictor69@gmail.com>
2024-04-04 17:41:15 +0300

    Add files via upload

---
+++ Day5/constructors.py
@@ -0,0 +1,14 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("Draw")
+
+
+point2 = Point(4, 5)
+print(point2.x)
