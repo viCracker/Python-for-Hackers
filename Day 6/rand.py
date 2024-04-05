fa3a1cdd91dbd9508e0cb55905d1e7dfd4b329af
Victor Mutiso <mutisovictor69@gmail.com>
2024-04-05 22:55:47 +0300

    Add files via upload

---
+++ Day 6/rand.py
@@ -0,0 +1,9 @@
+import random
+
+for i in range(3):
+    print(random.randint(10,1000))
+print(random.random())
+
+hackers = ["viCracker", "Steiner", "Kingpin", "TheeEclipse", "G3N7L3M4N", "Anonymous"]
+best = random.choice(hackers)
+print(best)
